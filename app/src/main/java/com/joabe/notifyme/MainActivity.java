99f488b7c7fe7790e9b906501cbc2da227fd7ba5
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-10-10 09:49:12 -0300

    Creating a notification channel

--- app/src/main/java/com/joabe/notifyme/MainActivity.java
+++ app/src/main/java/com/joabe/notifyme/MainActivity.java
@@ -2,11 +2,19 @@ package com.joabe.notifyme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+
+    private NotificationManager mNotifyManager;
 
     private Button button_notify;
 
@@ -26,5 +34,19 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendNotification() {
 
+    }
+
+    public void createNotificationChannel() {
+        mNotifyManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,
+                    "Mascot Notification", NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Mascot");
+            mNotifyManager.createNotificationChannel(notificationChannel);
+        }
     }
 }
