be96dd0c782a344e51c87d7b58e7dc34abe2969d
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-10-10 09:22:42 -0300

    Creating the layout

--- app/src/main/java/com/joabe/notifyme/MainActivity.java
+++ app/src/main/java/com/joabe/notifyme/MainActivity.java
@@ -3,12 +3,28 @@ package com.joabe.notifyme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button_notify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_notify = findViewById(R.id.notify);
+        button_notify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendNotification();
+            }
+        });
+    }
+
+    public void sendNotification() {
+
     }
 }
